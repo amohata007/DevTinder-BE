6e6e6b87ebcd8e9ff2366a86144c0e764798e94d
amohata007 <amohata.007@gmail.com>
2025-06-14 14:52:58 +0530

    Init Database

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const { connectDb } = require("./config/database.js");
+const User = require("./models/user.js");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  //creating a new instance of User model
+  const user = new User({
+    firstName: "MS",
+    lastName: "Dhoni",
+    age: 43,
+    gender: "Male",
+  });
+
+  try {
+    await user.save();
+    res.send("User Added Successfully");
+  } catch (err) {
+    res.status(400).send("Error while saving user: ", err.message);
+  }
+});
 
 connectDb()
   .then(() => {
