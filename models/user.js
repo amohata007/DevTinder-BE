5cdb303f89f0d54368176760dd3863c7377a6aa1
amohata007 <amohata.007@gmail.com>
2025-06-15 12:38:15 +0530

    Validators

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,10 +18,20 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email..!!");
+        }
+      },
     },
     password: {
       type: String,
       required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong Password..!!");
+        }
+      },
     },
     age: {
       type: Number,
